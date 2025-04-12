28c8e6d9fa9280be14880988a9b60a7d127e675f
Vinícius Dias <vinicius.dsilva@sptech.school>
2025-04-12 16:08:27 -0300

    arrumando login para deixar funcional

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -2,19 +2,16 @@ var database = require("../database/config")
 
 function autenticar(email, senha) {
     var instrucaoSql = `
-    SELECT 
-        e.idEmployer,
-        e.name,
-        e.role,
-        c.idCompany,
-        c.socialReason
-    FROM Employer AS e
-    JOIN Company AS c ON e.fkCompany = c.idCompany
-    JOIN Contact AS ct ON e.fkContact = ct.idContact
-    JOIN Login AS l ON l.fkEmployer = e.idEmployer
-    WHERE ct.email = '${email}'
-    AND l.password = '${senha}';`;
-
+        SELECT 
+            e.idEmployer,
+            e.name,
+            e.role,
+            e.fkCompany AS idCompany,
+            c.socialReason
+        FROM Employer AS e
+        JOIN Company AS c ON e.fkCompany = c.idCompany
+        WHERE e.email = '${email}' AND e.password = '${senha}';
+    `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }

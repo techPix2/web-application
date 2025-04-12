ee92fb0a590e74304a9d4c26d3f27662436e5fd0
Felipe-Ortega <felipe.osouza@sptech.school>
2025-04-12 16:16:34 -0300

    KPIs cientista finalizada

--- public/js/dasboardCientista.js
+++ public/js/dasboardCientista.js
@@ -239,3 +239,135 @@ function carregarGraficos(){
 
 
 
+const id = sessionStorage.ID_FUNCIONARIO;
+
+
+async function listarAlertasMaquinasPorQuadrante(fk_company, quadrante, periodo, tempo) {
+    let data;
+    try {
+        const response = await fetch(`/dashCientista/listarAlertasMaquinasPorQuadrante/${fk_company}/${quadrante}/${periodo}/${tempo}`);
+        data = await response.json();
+        return data
+    } catch (err) {
+        console.error(err);
+        return {
+            position: `${quadrante}`,
+            length: 0
+        };
+    }
+}
+
+
+function listarAlertasPorComponentePorMaquina(fk_company, quadrante, periodo, tempo){
+    fetch(`/dashCientista/listarAlertasPorComponentePorMaquina/${fk_company}/${quadrante}/${periodo}/${tempo}`,{
+        method:"GET"
+    }).then((res) => {
+        res.json().then((json)=>{
+            console.log(json)
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+async function carregarAlertasTodosQuadrantes(){
+    //Os dados estão chumbados, só alterar posteriormente para os dados do sessionStorage
+    fk_company = 1
+    fk_periodo = "DAY"
+    tempo = 15
+    const [q1, q2, q3, q4] = await Promise.all([
+        listarAlertasMaquinasPorQuadrante(fk_company, 1, fk_periodo, tempo),
+        listarAlertasMaquinasPorQuadrante(fk_company, 2, fk_periodo, tempo),
+        listarAlertasMaquinasPorQuadrante(fk_company, 3, fk_periodo, tempo),
+        listarAlertasMaquinasPorQuadrante(fk_company, 4, fk_periodo, tempo)
+    ]);
+    return [q1, q2, q3, q4];
+}
+
+async function verificarQuadranteComMaisAlertas(){
+    var quadrantes =  await carregarAlertasTodosQuadrantes()
+    console.log(quadrantes)
+    console.log(quadrantes.length)
+    let quadranteMaisAlertas = quadrantes[0]
+    for (let i = 1; i < quadrantes.length; i++) {
+        if (quadrantes[i].length > quadranteMaisAlertas.length){
+            quadranteMaisAlertas = quadrantes[i]
+        }
+    }
+    maisAlertasQuadranteKPI.innerHTML= quadranteMaisAlertas[0].position
+    return quadranteMaisAlertas
+}
+async function verificarDiaDaSemana(){
+    let quadrante = await verificarQuadranteComMaisAlertas()
+    const diaSemanaQuantidade= [0,0,0,0,0,0,0]
+    for (let i = 1; i < quadrante.length; i++) {
+        const data = new Date(quadrante[i].dateTime).getDay()
+        diaSemanaQuantidade[data]+=1
+    }
+    let maior = diaSemanaQuantidade[0]
+    for (let i = 1; i < quadrante.length; i++) {
+        if (diaSemanaQuantidade[i] > maior){
+            maior = diaSemanaQuantidade[i]
+        }
+    }
+    indice = diaSemanaQuantidade.indexOf(maior)
+    switch (indice) {
+        case 0:
+            maisAlertasDiaKPI.innerHTML = "Domingo"
+            break
+        case 1:
+            maisAlertasDiaKPI.innerHTML =  "Segunda"
+            break
+
+        case 2:
+            maisAlertasDiaKPI.innerHTML =  "Terça"
+            break
+
+        case 3:
+            maisAlertasDiaKPI.innerHTML =  "Quarta"
+            break
+
+        case 4:
+            maisAlertasDiaKPI.innerHTML =  "Quinta"
+            break
+
+        case 5:
+            maisAlertasDiaKPI.innerHTML =  "Sexta"
+            break
+
+        case 6:
+            maisAlertasDiaKPI.innerHTML =  "Sábado"
+            break
+    }
+}
+
+async function encontrarComponenteMaisFrequente() {
+    let quadrante = await verificarQuadranteComMaisAlertas()
+    const contador = {};
+
+    quadrante.forEach(item => {
+        const tipo = item.type;
+        if (tipo) {
+            contador[tipo] = (contador[tipo] || 0) + 1;
+        }
+    });
+
+    let tipoMaisFrequente = null;
+    let maiorContagem = 0;
+
+    for (const tipo in contador) {
+        if (contador[tipo] > maiorContagem) {
+            maiorContagem = contador[tipo];
+            tipoMaisFrequente = tipo;
+        }
+    }
+
+    maisAlertasComponente.innerHTML = tipoMaisFrequente;
+}
+
+
+function carregarDados() {
+    carregarGraficos();
+    nome_usuario.innerHTML = sessionStorage.NOME_USUARIO;
+}
--- public/js/teste.js
+++ public/js/teste.js
@@ -11,30 +11,3 @@ function listarServidorPorEmpresa(fk_company){
         console.log(err)
     })
 }
-
-function listarAlertasMaquinasPorQuadrante(fk_company, id_server, periodo, tempo){
-    fetch(`/dashCientista/listarAlertasMaquinasPorQuadrante/${fk_company}/${id_server}/${periodo}/${tempo}`,{
-        method:"GET"
-    }).then((res) => {
-        res.json().then((json)=>{
-            console.log(json)
-        });
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-}
-
-
-function listarAlertasPorComponentePorMaquina(fk_company, id_server, periodo, tempo){
-    fetch(`/dashCientista/listarAlertasPorComponentePorMaquina/${fk_company}/${id_server}/${periodo}/${tempo}`,{
-        method:"GET"
-    }).then((res) => {
-        res.json().then((json)=>{
-            console.log(json)
-        });
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-}

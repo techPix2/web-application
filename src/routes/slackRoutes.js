d403b810340782318ab8115598d837ba9cc37c53
Lara-Soares <lara.soares@sptech.school>
2025-06-02 11:50:42 -0300

    integração slack funcionando

--- src/routes/slackRoutes.js
+++ src/routes/slackRoutes.js
@@ -9,6 +9,7 @@ const channelId = "C08RST7LY1H";
 //buscar mensagens
 router.get('/mensagens', async (req, res) => {
   try {
+    console.log("Buscando mensagens do Slack...");
     const now = Math.floor(Date.now() / 1000);
     const yesterday = now - 24 * 60 * 60; 
     const result = await web.conversations.history({
@@ -17,13 +18,16 @@ router.get('/mensagens', async (req, res) => {
       oldest: yesterday
     });
     res.json(result.messages.reverse());
+    if (result.status === 200) {
+      console.log(`Mensagens recebidas com sucesso! Total: ${result.messages.length}`);
+    }
   } catch (err) {
     res.status(500).json({ error: 'Erro ao buscar mensagens do Slack' });
   }
 });
 
 //enviar mensagem
-router.post('/mensagens', async (req, res) => {
+router.post('/enviarMensagem', async (req, res) => {
   const { text } = req.body;
   try {
     const result = await web.chat.postMessage({ channel: channelId, text });

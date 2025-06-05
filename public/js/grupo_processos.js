9773a8554295fa475f91a3dc44139eabab61df25
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2025-06-05 20:29:52 -0300

    a

--- public/js/grupo_processos.js
+++ public/js/grupo_processos.js
@@ -123,4 +123,3 @@ export const grupoProcessos = {
             "node(1)"
         ]
     }
-
--- src/routes/dashAnalista.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashAnalistaController = require("../controllers/dashAnalistaController");
-
-router.get("/servidores/:fk_company", dashAnalistaController.listarServidores);
-
-module.exports = router;
